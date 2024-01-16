0b034b416ce1035f8c4d43a1809d5d2d7a230d1b
UmutKaradeniz <ukaradeniz21@gmail.com>
2024-01-16 15:58:39 +0100

    added register_restaurant and render for Home page

--- website/auth.py
+++ website/auth.py
@@ -3,6 +3,11 @@ from db_funcs import *
 
 auth = Blueprint('auth', __name__)
 
+#function/route to render home page
+@auth.route('/', methods = ['GET', 'POST'])
+def homepage():
+    return render_template("home.html")
+
 #function/route to register customer
 @auth.route('/register_customer', methods=['GET', 'POST'])
 def registerCustomer():
@@ -18,4 +23,19 @@ def registerCustomer():
         else:
             flash('Username already exists. Please choose a different username.', category = 'error')
     
+    return render_template("sign-up.html")
+
+#function/route to restaurant customer
+@auth.route('/register_restaurant', methods=['GET', 'POST'])
+def registerRestaurant():
+    if request.method == 'POST':
+        res_name = request.form.get('res_name')
+        address = request.form.get('address')
+        postcode = request.form.get('postcode')
+        password = request.form.get('password')
+        if DBfuncs.registerRestaurant(res_name, address, postcode, password) is not False:
+            flash('Registration successful.', category = 'success')
+        else:
+            flash('Restaurant name already exists. Please choose a different username.', category = 'error')
+        
     return render_template("sign-up.html")
