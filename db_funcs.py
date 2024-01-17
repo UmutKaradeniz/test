89730e4e8f22cc6bb441dd7148aa0ca1e0790666
UmutKaradeniz <ukaradeniz21@gmail.com>
2024-01-17 13:51:12 +0100

    changes

--- db_funcs.py
+++ db_funcs.py
@@ -21,7 +21,7 @@ class DBfuncs:
                 address TEXT NOT NULL,
                 postcode INTEGER NOT NULL,
                 password TEXT NOT NULL,
-                picture BLOB                           
+                picture BLOB                          
             )""")
         cur.execute("""CREATE TABLE IF NOT EXISTS menu_items(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,6 +31,11 @@ class DBfuncs:
                 type TEXT CHECK(TYPE IN('Main', 'Side', 'Desert', 'Drink')) NOT NULL,
                 price INTEGER NOT NULL,
                 FOREIGN KEY(res_id) REFERENCES restaurants(id)       
+            )""")
+        cur.execute("""CREATE TABLE IF NOT EXISTS allowed_postcode(
+                res_id INTEGER NOT NULL,
+                postcode INTEGER NOT NULL,  
+                FOREIGN KEY(res_id) REFERENCES restaurants(id)               
             )""")
         con.commit()
         con.close()
@@ -97,24 +102,56 @@ class DBfuncs:
         except sql.Error as Err:
             print("SQLite Error: ", Err)
             return False
-        
-    #user finder
-    def findUser(username):
+    
+    #returns id from the row of the given username
+    def getIDRestaurant(username):
         con = sql.connect('database.db')
         cur = con.cursor() 
-        cur.execute("SELECT * FROM restaurants WHERE res_name=?", (username))
-        user_info = cur.fetchone()
-        if user_info[0] is False:
-            cur.execute("SELECT * FROM customers WHERE username=?", (username))
-            user_info = cur.fetchone()
-            if user_info[0] is False:   
-                print("no match found")
+        cur.execute("SELECT id FROM restaurants WHERE res_name=?", (username))
+        id = cur.fetchone()
+        return id
+    
+    def getIDCustomer(username):
+        con = sql.connect('database.db')
+        cur = con.cursor() 
+        cur.execute("SELECT id FROM customers WHERE username=?", (username))
+        id = cur.fetchone()
+        return id
+    
+    def addNewPostcode(res_id, postcode):
+        try:
+            con = sql.connect('database.db')
+            con.execute("PRAGMA foreign_keys = ON")
+            cur = con.cursor() 
+            cur.execute("INSERT INTO allowed_postcode VALUES (?, ?)", (res_id, postcode))
+            con.commit()
+            con.close()
+            return True
+        except sql.Error as Err:
+            print("SQLite Error: ", Err)
+            return False 
+        
+    def retrieveResData(id):
+        con = sql.connect('database.db')
+        cur = con.cursor()
+        cur.execute("""SELECT res_name, address, postcode FROM restaurants WHERE id IN
+                        (SELECT res_id FROM allowed_postcode WHERE postcode 
+                        IN (SELECT postcode FROM customers WHERE id = ?))""", (id, ))
+        data = cur.fetchall()
+        print(data)
         con.commit()
         con.close()
-        return user_info
-    
-# if __name__ == "__main__":
-#     DBfuncs.createTables()
-#     DBfuncs.registerCustomer("Karadeniz2", "Umut", "ABC 6", 47055, "Umut_Karadeniz", 1234554321)
-#     DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321)
-#     DBfuncs.addNewItem(3, "hamburger", "meat", "Main", 20)
+        return data
+
+if __name__ == "__main__":
+    DBfuncs.createTables()
+    # DBfuncs.registerCustomer("Karadeniz2", "Umut", "ABC 6", 47055, "Umut_Karadeniz", 1234554321)
+    # DBfuncs.registerRestaurant("Al-Basha2", "ABC 7", 47055, 123321)
+    # DBfuncs.addNewItem(3, "hamburger", "meat", "Main", 20)
+    # DBfuncs.addNewPostcode(1, 12345)
+    # DBfuncs.addNewPostcode(2, 12345)
+    # DBfuncs.addNewPostcode(3, 12345)
+    # DBfuncs.addNewPostcode(1, 23456)
+    # DBfuncs.addNewPostcode(2, 23456)
+    DBfuncs.retrieveResData(4)
+    DBfuncs.retrieveResData(3)
